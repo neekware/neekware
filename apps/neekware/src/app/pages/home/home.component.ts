2910cc70cf765d019e3bf44a5f2ed112acb69f2a
Val Neekman <val@neekware.com>
2022-04-13 14:25:01 -0400

    clean up

--- apps/neekware/src/app/pages/home/home.component.ts
+++ apps/neekware/src/app/pages/home/home.component.ts
@@ -24,24 +24,28 @@ export class HomeComponent implements OnInit, OnDestroy {
   logoSize = 'large';
   pitchList = [
     {
-      title: _('PITCH.CHAT.MANAGER.TITLE'),
-      description: _('PITCH.CHAT.MANAGER.DESCRIPTION'),
-      image: '/assets/images/misc/chat-manager-overlay.png',
+      title: 'Who we are!',
+      description:
+        'Neekware has been in the business of dynamic & data-driven web development and services since 2008. We believe that every website & application should be designed for the future but built for today. This way we can keep the upfront cost down while allowing you to grow your business at your own pace. "Design it right!" is the name of the game.',
+      image: '/assets/images/misc/neekware-card.png',
     },
     {
-      title: _('PITCH.CHAT.MANAGER.ADMIN.TITLE'),
-      description: _('PITCH.CHAT.MANAGER.ADMIN.DESCRIPTION'),
-      image: '/assets/images/misc/chat-manager-admin.png',
+      title: 'Single code-base, multiple platforms',
+      description:
+        'We build for all platforms, including web, mobile, desktop, and IoT.  Deployment will be easy, as we have a robust set of tools to help you get started. We hold a strong focus on security, performance, and scalability. We are always looking for new ways to improve our tools and ultimately your products.',
+      image: '/assets/images/misc/platforms-card.png',
     },
     {
-      title: _('PITCH.ANNOTATE.TITLE'),
-      description: _('PITCH.ANNOTATE.DESCRIPTION'),
-      image: '/assets/images/misc/annotation.png',
+      title: 'What can we do for you?',
+      description:
+        "We can help you at all stages, from the concept to the launch of your product & services. Simply tell us your requirements, schedule & budget. We will provide you with a multi-release timeline that maps onto your schedule. This way, you can choose the features you'd like to see, included in each release.",
+      image: '/assets/images/misc/whatwedo-card.png',
     },
     {
-      title: _('PITCH.PLATFORMS.TITLE'),
-      description: _('PITCH.PLATFORMS.DESCRIPTION'),
-      image: '/assets/images/misc/platforms.png',
+      title: 'Our technology stack',
+      description:
+        'We use the latest technologies to build the best products for you. Our stack is built with mono-repositories, so you can easily develop and deploy multiple applications easily. We use Typescript, throughout our stack, the frontend, backend, and infrastructure. We can build teams for you, train the staff, and provide ongoing consulting services. We hold your hands throughout the process, and beyond!',
+      image: '/assets/images/misc/techstack-card.png',
     },
   ];
 
