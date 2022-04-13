921df3a32d3c8b36998b4789c6216a453eba5234
Val Neekman <val@neekware.com>
2022-04-13 17:43:22 -0400

    favicon

--- apps/neekware/src/app/pages/about/about.component.ts
+++ apps/neekware/src/app/pages/about/about.component.ts
@@ -19,13 +19,13 @@ export class AboutComponent {
     {
       title: 'Val Neekman',
       description:
-        'Val is responsible for defining the overall business strategy for the firm, including time allocated to sales and operations. Val brings more than 20 years of experience to the firm. He previously served as founder and CEO of Neekware Inc, which he pivoted to a software consultancy firm in 2008. Val holds a bachelor of engineering from Carleton University. Val lives in Ontario Canada.',
+        'Val is responsible for defining the overall business strategy for the firm, including time allocated to sales and operations. Val brings more than 20 years of experience to the firm. He previously served as founder and CEO of Neekware Inc, which he pivoted to a software consultancy firm in 2008. Val holds a bachelor of engineering from Carleton University. Val lives in Ontario, Canada.',
       image: '/assets/images/people/ceo-card.png',
     },
     {
       title: 'Cathy H. Liu',
       description:
-        'Cathy is responsible for managing the accounts and billing of our clients. She has over 10 years of experience in the industry. She holds a bachelor of computer engineering from Beijing University. She lives in Ontario Canada.',
+        'Cathy is responsible for managing the accounts and billing of our clients. She has over 10 years of experience in the industry. She holds a bachelor of computer engineering from Beijing University. She lives in Ontario, Canada.',
       image: '/assets/images/people/account-manager-card.png',
     },
   ];
